b2940059578539603a833caf61eae3f6b23f5f57
Alon Lavi <alon@argus-sec.com>
2017-01-28 22:36:44 +0200

    configurable

--- src/main/java/org/shayalon/MessageDecrypt.java
+++ src/main/java/org/shayalon/MessageDecrypt.java
@@ -3,26 +3,12 @@ package org.shayalon;
 import org.shayalon.models.Configuration;
 import org.shayalon.utils.EncTools;
 import org.shayalon.utils.XmlTools;
-import org.xml.sax.SAXException;
-
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerException;
-import java.io.IOException;
-import java.security.*;
-import java.security.cert.CertificateException;
 
 public class MessageDecrypt {
 
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, TransformerException {
+    public static void main(String[] args) throws Exception {
         Configuration configuration = Configuration.fromXml(XmlTools.readFile("config.xml"));
-        try {
-            EncTools.decryptAndVerifySignature(configuration);
-        } catch (CertificateException | NoSuchAlgorithmException | UnrecoverableEntryException | KeyStoreException | BadPaddingException | IllegalBlockSizeException | InvalidKeyException | NoSuchPaddingException | NoSuchProviderException | SignatureException | InvalidAlgorithmParameterException e) {
-            e.printStackTrace();
-        }
+        EncTools.decryptAndVerifySignature(configuration);
     }
 
 }
